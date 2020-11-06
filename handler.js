75c606c3c0edf46579b15ca2a2eef7623c680353
Byron Buckley <3874813+komplexb@users.noreply.github.com>
2020-11-06 17:38:51 +1100

    device code auth works - successful happy path - got data!

--- handler.js
+++ handler.js
@@ -1,22 +1,23 @@
 const {deviceLogin} = require('./lib/auth');
 const {getRandomNote} = require('./lib/onenote');
+const {push} = require('./lib/pushbullet');
 
 const app = async (event, context) => {
   // return push
-  deviceLogin()
-  /* return getRandomNote()
+  // await deviceLogin()
+  return getRandomNote()
     .then((note) => {
       if (typeof note === 'undefined') {
         throw new Error()
       } else {
-        console.log(note)
-        // push(note)
+        // console.log(note)
+        push(note)
       }
     })
     .catch((err) => {
       console.log('Ooops!', `Can't seem to find any notes here. Please check if you created a section called '${process.env.NOTIFYER_SECTION}', add some notes.`)
       console.error('app', err)
-    }) */
+    })
 };
 
 module.exports = {
