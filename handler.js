d73f893f9415dbc2b07f13ce5c008214fa68aa87
Byron Buckley <3874813+komplexb@users.noreply.github.com>
2020-11-13 19:10:42 +1100

    handler.js: its good practice to return status

--- handler.js
+++ handler.js
@@ -1,25 +1,48 @@
 const {deviceLogin, hasValidToken} = require('./lib/auth');
 const {getRandomNote} = require('./lib/onenote');
 const {notify} = require('./lib/pushbullet');
+const { promises: fs } = require("fs");
+
+function initCache() {
+  /* fs.copyFile('./tmp/onenote', '/tmp/onenote')
+  .then(() => console.log('"onenote" was copied to tmp'))
+  .catch(() => console.log('"onenote" could not be copied'))
+
+  fs.copyFile('./tmp/cache.json', '/tmp/cache.json')
+  .then(() => console.log('"cache.json" was copied to tmp'))
+  .catch(() => console.log('"cache.json" could not be copied')) */
+}
 
 const app = async (event, context) => {
+  if(process.env.STAGE === 'prod') {
+    initCache()
+  }
+
   if(!hasValidToken()) {
     await deviceLogin()
   }
 
-  return getRandomNote()
+  const resp = await getRandomNote()
     .then((note) => {
       if (typeof note === 'undefined') {
         throw new Error()
       } else {
-        // console.log(note)
-        return notify(note)
+        return notify(note);
       }
     })
     .catch((err) => {
       console.log('Ooops!', `Can't seem to find any notes here. Please check if you created a section called '${process.env.NOTIFYER_SECTION}', add some notes.`)
       console.error('app', err)
+      return {
+        err
+      }
     })
+
+  return {
+    status: resp.status,
+    title: resp.body.title,
+    body: resp.body.body
+  }
 };
 
 module.exports = {
