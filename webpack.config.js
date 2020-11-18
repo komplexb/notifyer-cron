bffaa98903aff08f5e8ec2df8d1afee013e9d20d
Byron Buckley <3874813+komplexb@users.noreply.github.com>
2020-11-18 17:41:06 +1100

    webpack: optimize cold starts bundle size: ~6mb to 1.4 start time: ~16s to ~10 https://www.gorillastack.com/news/optimizing-your-lambda-cold-starts-with-serverless-webpack/

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+// webpack.config.js
+const path = require('path')
+const slsw = require('serverless-webpack')
+const CopyPlugin = require('copy-webpack-plugin')
+
+module.exports = {
+  entry: slsw.lib.entries,
+  target: 'node',
+  mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
+  stats: 'minimal',
+  devtool: 'nosources-source-map',
+  performance: {
+    hints: false
+  },
+  resolve: {
+    extensions: ['.js', '.json']
+  },
+  output: {
+    libraryTarget: 'commonjs2',
+    path: path.join(__dirname, '.webpack'),
+    filename: '[name].js',
+    sourceMapFilename: '[file].map'
+  },
+  optimization: {
+    minimize: false
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: 'tmp/**/*' }]
+    })
+  ]
+}
