5b4c5c787342c81cd8b3688810e899e6164f5168
Byron <3874813+komplexb@users.noreply.github.com>
2022-09-04 21:32:02 +1000

    Feature/multisection multichannel (#3)
    * part 1: works with 2nd brain notebook
    
    * part 1: works with 2nd brain notebook
    - comments and minor refactors
    
    * part 2: use messageSettings
    - toggle previews
    - use prefix
    - specify channel
    
    * bug: skip logging recent notes if notes collection less than recent notes constant
    
    * multisection-multichannel: milestone 1
    
    * multisection-multichannel: bugs
    - update event yml
    - fix local storage persistence issues: note counts out of sync
    - robust support for finding sections across notebooks
    
    * multisection-multichannel: fix telegram markdown formatting issue
    - refactors and cleanup
    
    * multisection-multichannel: add weekday wellness
    - update schedules
    - additional logging
    
    * multisection-multichannel: milestone 2
    onenote to markdown message formatting
    
    * multisection-multichannel: milestone 2
    onenote to markdown message formatting
    
    * multisection-multichannel: underline as highlight
    
    * fix: escape telegram reserved character '-'
    
    * fix: unreliably removing images from html crashes app on telegram push
    - update node-html-parser version to resolve above
    - update schedule
    
    * multisection-multichannel: tweak schedules

--- handler.js
+++ handler.js
@@ -4,12 +4,13 @@ const notify = require('./lib/notify')
 const localStorage = require('./lib/store')
 const { promises: fs } = require('fs')
 const db = require('./db/persist')
+const { snakeCase } = require("snake-case");
 
 /**
  * Lambda functions have ephemeral storage on the server in /tmp.
  * Seed the MSAL Key Cache and localStorage with the latest from the database
  */
-async function initCache(sectionName) {
+async function initCache(sectionHandle) {
   // populate cache with db contents
   const data = await db.getItem('cache')
   await fs
@@ -18,40 +19,48 @@ async function initCache(sectionName) {
 
   // populate local storage with login contents
   // coerced to json
+  localStorage.initStore();
   const onenote = await db.getItem('onenote', true)
   localStorage.setItem('onenote', onenote)
 
-  const count = await db.getItem(`${sectionName.toLowerCase()}_section_count`)
-  localStorage.setItem(`${sectionName.toLowerCase()}_section_count`, count)
+  const count = await db.getItem(`${sectionHandle}_section_count`)
+  localStorage.setItem(`${sectionHandle}_section_count`, count)
 
-  const recent =
-    (await db.getItem(`recent_${sectionName.toLowerCase()}`, true)) || []
-  localStorage.setItem(`recent_${sectionName.toLowerCase()}`, recent)
+  const recent = (await db.getItem(`recent_${sectionHandle}`, true)) || []
+  localStorage.setItem(`recent_${sectionHandle}`, recent)
 
-  console.log('Restore localStorage')
+  console.log('Restore localStorage', `${sectionHandle}: ${count}`, `recent: ${recent.length}`)
 }
 
 const app = async (event, context) => {
-  const sectionName = event.section || process.env.NOTIFYER_SECTION
+  let { onenoteSettings, messageSettings } = event
 
-  await initCache(sectionName) // setup the files needed for the app to work
+  onenoteSettings = {
+    sectionHandle: snakeCase(onenoteSettings.sectionName),
+    ...onenoteSettings
+  }
+
+  await initCache(onenoteSettings.sectionHandle) // setup the files needed for the app to work
   await refreshToken() // refresh the tokens needed for MS Graph Calls
 
   if (!hasValidToken()) {
     await deviceLogin()
   }
 
-  const resp = await getRandomNote(sectionName)
+  const resp = await getRandomNote(onenoteSettings)
     .then(note => {
       if (typeof note === 'undefined') {
         throw new Error()
       }
-      return notify.withTelegram(note, sectionName === 'Verses' ? '📖' : '💡')
+      return notify.withTelegram(
+        note,
+        messageSettings
+      )
     })
     .catch(err => {
       console.log(
         'Ooops!',
-        `Can't seem to find any notes here. Please check if you created a section called '${sectionName}', add some notes.`
+        `Can't seem to find any notes here. Please check if you created a section called '${onenoteSettings.sectionName}', add some notes.`
       )
       console.error('App: Check Logs', err)
       return {
